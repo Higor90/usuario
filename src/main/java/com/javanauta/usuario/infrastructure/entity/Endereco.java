0e0072906254745869a669ebcfb75ffc6d79a44b
Higor <nobregahigor@gmail.com>
2026-05-20 14:38:32 -0300

    adicionados metodos de gravar endereço e telefone do usuario

--- src/main/java/com/javanauta/usuario/infrastructure/entity/Endereco.java
+++ src/main/java/com/javanauta/usuario/infrastructure/entity/Endereco.java
@@ -3,8 +3,6 @@ package com.javanauta.usuario.infrastructure.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.List;
-
 @Getter
 @Setter
 @AllArgsConstructor
@@ -17,15 +15,19 @@ public class Endereco {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "rua")
     private String rua;
+    @Column(name = "numero")
     private Long numero;
+    @Column(name = "complemento", length = 10)
     private String complemento;
+    @Column(name = "cidade", length = 150)
     private String cidade;
+    @Column(name = "estado", length = 2)
     private String estado;
+    @Column(name = "cep", length = 9)
     private String cep;
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "usuario_id", referencedColumnName = "id")
-    private List<Telefone> telefones;
-
+    @Column(name = "usuario_id")
+    private Long usuario_id;
 
 }
